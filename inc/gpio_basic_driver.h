ee7ec88ab4e8f2918112def6608ad5945450a702
Abhishek JP <pushpan4267@gmail.com>
2026-03-20 09:30:36 +0530

    Add files via upload

---
+++ inc/gpio_basic_driver.h
@@ -0,0 +1,18 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <stdint.h>
+
+/* ===================== GPIO MODES ===================== */
+typedef enum {
+    GPIO_INPUT = 0,
+    GPIO_OUTPUT
+} gpio_mode_t;
+
+/* ===================== FUNCTION PROTOTYPE ===================== */
+void gpio_init(uint8_t pin, gpio_mode_t mode);
+void gpio_write(uint8_t pin, uint8_t value);
+uint8_t gpio_read(uint8_t pin);
+void gpio_toggle(uint8_t pin);
+
+#endif
